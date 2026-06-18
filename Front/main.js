c040bfdc5a56b3c2103a344d243bb5277b8056de
TkI3 <t13k9.243@gmail.com>
2026-06-18 22:57:12 +0100

    Amelioration du systeme de filtre

--- Front/main.js
+++ Front/main.js
@@ -14,12 +14,12 @@ async function chargeannonces() {
 
     contenair.innerHTML +=
       `<div class="carte_produit">
-        <p style="display: none">${prix}</p>
+        <p style="display: none">${localisation}</p>
         <div class="cnt_img"><img src="${image_link}" alt="${titre}" loading="lazy">
           <button class="fav"><i class="fas fa-heart"></i></button>
         </div>
         <div class="info"><p class="titre">${titre}</p><p class="prix">${prix} Fc</p>
-          <p class="locate"><i class="fas fa-crosshairs"></i> ${localisation}</p></div>
+          <p class="Localisation"><i class="fas fa-crosshairs"></i>${localisation}</p></div>
       </div>`;
 
   });
@@ -31,28 +31,46 @@ function filtrage(){
   let filtre_actif = document.getElementsByClassName("active");
   filtre_actif = filtre_actif[0].textContent;
   
+  let all_carte = document.querySelectorAll('.carte_produit');
 
-  let all_carte = document.querySelectorAll('.carte_produit')
+  let categorie = document.getElementById("select");
+  categorie = categorie.options[select.selectedIndex].text;
 
   all_carte.forEach(carte => {
 
-    const prix = carte.querySelector("p").textContent;
-      
-    if(filtre_actif === "Nioso"){
-      carte.style.display = "block";
+  const delta = carte.querySelector("."+categorie).textContent;
 
-    }else{
-      carte.style.display = prix === filtre_actif ?  "block" : "none";
-    }
+  console.log(delta);
+      
+  if(filtre_actif === "Nioso"){
+    carte.style.display = "block";
+
+  }else{
+    carte.style.display = delta == filtre_actif ? "block" : "none";
+  }
     
   })
 }
 
+function categorisation(){
 
-chargeannonces();
+  
+  const seleact = document.getElementById("select");
+  const filtres  = document.querySelectorAll(".categorieDfiltre");
+
+  filtres.forEach(el =>{ el.style.display = "none"});
+
+  const valeur = seleact.value;
+  const cible = document.getElementById(valeur);
+  cible.style.display = ""; 
+
+  let all_carte = document.querySelectorAll('.carte_produit')
+
+  all_carte.forEach(carte => {carte.style.display = "block";})
+
+}
 
 const filters = document.querySelectorAll(".filter");
-
 filters.forEach(btn => {
   btn.addEventListener("click", () => {
     filters.forEach(b => 
@@ -64,4 +82,8 @@ filters.forEach(btn => {
   
 });
 
+document.getElementById("logos").addEventListener("click",() => { window.location.href = "/"})
+document.getElementById("login").addEventListener('click',() => { window.location.href = "/Connexion.html"})
+document.getElementById("select").addEventListener("change", categorisation);
 
+chargeannonces();
